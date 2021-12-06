4cac8007e985d3eb6f141775697a408445503579
Rahul Suwasiya <88681527+rsuwasiya17@users.noreply.github.com>
2021-12-06 09:47:13 +0530

    Update data_quality.py

--- plugins/operators/data_quality.py
+++ plugins/operators/data_quality.py
@@ -10,15 +10,28 @@ class DataQualityOperator(BaseOperator):
     @apply_defaults
     def __init__(self,
                  redshift_conn_id = "",
-                 tables=[],
+                 dq_test = "",
+                 tables = [],
                  *args, **kwargs):
 
         super(DataQualityOperator, self).__init__(*args, **kwargs)
         self.tables = tables
         self.redshift_conn_id = redshift_conn_id
+        self.dq_test = dq_test
 
     def execute(self, context):
         redshift_hook = PostgresHook(self.redshift_conn_id)
+        for test in self.dq_test:
+            sql = check.get('test_sql')
+            exp_result = check.get('expected_result')
+            records_query = redshift.get_records(sql)[0]
+            if exp_result != records_query[0]:
+                error_count += 1
+                failed_tests.append(sql)
+        if error_count > 0:
+            self.log.info('Tests failed')
+            self.log.info(failed_tests)
+            raise ValueError('Data quality check failed')
         for table in self.tables:
             self.log.info(f"Data Quality for {table} table")
             records = redshift_hook.get_records(f"SELECT COUNT(*) from {table}")
